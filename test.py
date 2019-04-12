b89945f75e14f2348aa63b6d1e70f120e133519f
champchun <champ.chun@gmail.com>
2019-04-12 12:48:59 +0800

    新增ECG

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
-import numpy as np
+from PIL import Image
+import face_recognition
+import cv2
 
-a = np.array([1, 2, 3])
-b = np.array([1, 2, 3])
+image = face_recognition.load_image_file("aa.jpg")
 
-print(a+b)
+face_locations = face_recognition.face_locations(image)
+
+image1 = image*1
+image1[:, :, 0] = image[:, :, 2]
+image1[:, :, 2] = image[:, :, 0]
+for (A, B, C, D) in face_locations:
+    cv2.rectangle(image1, (D, A), (B, C), (0, 255, 0), 2)
+cv2.imshow('image', image1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
